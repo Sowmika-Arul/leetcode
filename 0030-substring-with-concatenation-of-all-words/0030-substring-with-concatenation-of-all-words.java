2677d05c0a161c2d44e02cec9675cf13fc0af562
Sowmika Arulkumar <sowmikaarulkumar@gmail.com>
2025-04-14 21:22:09 +0530

    Time: 12 ms (69.58%), Space: 45.6 MB (58.84%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) return result;
+
+        int wordLength = words[0].length();
+        int numWords = words.length;
+        int totalLength = wordLength * numWords;
+
+        
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        
+        for (int i = 0; i < wordLength; i++) {
+            int left = i, right = i;
+            int count = 0; 
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            while (right + wordLength <= s.length()) {
+                String word = s.substring(right, right + wordLength);
+                right += wordLength;
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLength);
+                        left += wordLength;
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        count--;
+                    }
+
+                    
+                    if (count == numWords) {
+                        result.add(left);
+                    }
+                } else {
+                    
+                    windowMap.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findSubstring("barfoothefoobarman", new String[]{"foo", "bar"})); 
+        System.out.println(solution.findSubstring("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "word"})); // []
+    }
+}
