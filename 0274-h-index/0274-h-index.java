4ec5b9e55c8294d1ada9c581e13b20050b88153c
Sowmika Arulkumar <sowmikaarulkumar@gmail.com>
2025-06-09 20:36:04 +0530

    Time: 2 ms (69.4%), Space: 42.1 MB (7.57%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,19 +1,14 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int papers = citations.length;
-        int[] citationBuckets = new int[papers + 1];
-
-        for (int citation : citations) {
-            citationBuckets[Math.min(citation, papers)]++;
-        }
-
-        int cumulativePapers = 0;
-        for (int hIndex = papers; hIndex >= 0; hIndex--) {
-            cumulativePapers += citationBuckets[hIndex];
-            if (cumulativePapers >= hIndex) {
-                return hIndex;
-            }
-        }
-        return 0;        
+         int length = citations.length, count = 0;
+         Arrays.sort(citations);
+         int h;
+        for(int i = 0 ;i < length; i++)
+        {
+            h = length - i;
+            if(citations[i] >= h)
+            return h;
+        } 
+       return 0; 
     }
 }
