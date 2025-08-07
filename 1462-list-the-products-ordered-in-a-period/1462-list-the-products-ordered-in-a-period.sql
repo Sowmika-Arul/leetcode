6e90c86043ca28d5c22fb7894aa7c3c2b8f596f4
Sowmika Arulkumar <sowmikaarulkumar@gmail.com>
2025-08-07 22:57:41 +0530

    Time: 684 ms (70.58%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,5 @@
+select p.product_name, sum(o.unit) as unit from Products p join Orders o 
+on p.product_id = o.product_id 
+where month(o.order_date) = 2 and year(o.order_date) = '2020'
+group by p.product_id
+having unit >= 100;
